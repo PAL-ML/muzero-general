4844b3db4168abed81b5c1643601dd7041ae058b
Khush Jammu <khushjammu@gmail.com>
2021-07-20 19:02:32 +0800

    debugging

--- self_play.py
+++ self_play.py
@@ -47,6 +47,7 @@ class SelfPlay:
             shared_storage.get_info.remote("terminate")
         ):
             self.model.set_weights(ray.get(shared_storage.get_info.remote("weights")))
+            self.model.to(self.device)
             print("playloop start")
 
             if not test_mode:
